ceac0a3075d6c48602cb2b69e794f33d1b3cb5e7
ABDUL-Malik897 <malikabdulxd@gmail.com>
2026-06-21 02:36:58 +0530

    preparing for deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const api = axios.create({
     baseURL:
     process.env.NODE_ENV === "production"
-        ? "https://YOUR-RENDER-URL.onrender.com"
+        ? "https://visitorpass-project.onrender.com"
         : "",
 });
 
